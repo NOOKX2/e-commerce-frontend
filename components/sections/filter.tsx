b83023a09845ffd35d20e694b87bd84e08f4515a
NOOKX2 <iamthatchavit@gmail.com>
2025-12-21 18:16:03 +0700

    refactor: rename navbar to Nabar and filter to Filter for consistency

--- components/sections/filter.tsx
+++ components/sections/filter.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 
